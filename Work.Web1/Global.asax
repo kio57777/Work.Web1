1e0f63d6cf896f706034870917827d2d71cdef88
kio <kio@kingwaytek.com>
2016-03-10 23:03:58 +0800

    第一版版控

---
+++ Work.Web1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Work.Web1.MvcApplication" Language="C#" %>
---
+++ Work.Web1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Work.Web1.Startup))]
+namespace Work.Web1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
